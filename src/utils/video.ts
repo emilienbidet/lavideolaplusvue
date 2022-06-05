20d0600d3186073f8f2c658109fa14503f808ecc
Emilien Bidet <emilienbdt@gmail.com>
2022-06-05 18:32:25 +0200

    Fix the bug of duplicate video

--- src/utils/video.ts
+++ src/utils/video.ts
@@ -2,7 +2,14 @@ import {TVideo} from "../typings";
 
 import videos from "../assets/json/videos.json";
 
+let lastVideo: TVideo | null = null;
+
 export const getRandomVideo: () => TVideo = () => {
-    const video = videos[Math.floor(Math.random() * videos.length)];
-    return video;
+    const pickableVideos: TVideo[] = videos.filter(
+        (video: TVideo) => video !== lastVideo
+    );
+    const randomIndex = Math.floor(Math.random() * pickableVideos.length);
+    const randomVideo = pickableVideos[randomIndex];
+    lastVideo = randomVideo;
+    return randomVideo;
 };
